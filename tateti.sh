9a80feee01f44b7a1e9a33b635412a4704954140
Emmanuel P. Belascuain <epbelascuain@hotmail.com>
2020-04-01 11:32:03 -0300

    Versión 2.0 - Reingeniería completa

--- tateti.sh
+++ tateti.sh
@@ -1,10 +1,11 @@
-puntaje1=0
-puntaje2=0
-tablero=(1 2 3 4 5 6 7 8 9)
-jugando=true
-partida=true
+#!/bin/bash
 
 function mostrarTablero {
+	if [[ $turno -eq 0 ]]; then
+		echo "Turno del Jugador 1."
+	else
+		echo "Turno del Jugador 2."
+	fi
 	echo " ${tablero[0]} | ${tablero[1]} | ${tablero[2]}"
 	echo "-----------"
 	echo " ${tablero[3]} | ${tablero[4]} | ${tablero[5]}"
@@ -18,101 +19,95 @@ function marcarCasilla {
 	else 
 		if [[ $turno -eq 0 ]]; then
 			tablero[$1-1]="X"
-			turno=1
 		else
 			tablero[$1-1]="O"
-			turno=0
 		fi
 	fi
 }
 function checkResultado {
 	if [[ ${tablero[0]} == "$1" && ${tablero[1]} == "$1" && ${tablero[2]} == "$1" || ${tablero[3]} == "$1" && ${tablero[4]} == "$1" && ${tablero[5]} == "$1" || ${tablero[6]} == "$1" && ${tablero[7]} == "$1" && ${tablero[8]} == "$1" || ${tablero[0]} == "$1" && ${tablero[3]} == "$1" && ${tablero[6]} == "$1" || ${tablero[1]} == "$1" && ${tablero[4]} == "$1" && ${tablero[7]} == "$1" || ${tablero[2]} == "$1" && ${tablero[5]} == "$1" && ${tablero[8]} == "$1" || ${tablero[0]} == "$1" && ${tablero[4]} = "$1" && ${tablero[8]} == "$1" || ${tablero[2]} == "$1" && ${tablero[4]} == "$1" && ${tablero[6]} == "$1" ]]; then
-		echo "El jugador $2 ha ganado."
-		puntaje$2=$(($puntaje$2+1))
-		partida=false
-
-		echo "funciona"
+		if [[ $turno -eq 0 ]]; then
+			echo "Ganó el jugador 1."
+			puntaje[0]=$((${puntaje[0]}+1))
+		else
+			echo "Ganó el jugador 2."
+			puntaje[1]=$((${puntaje[1]}+1))
+		fi
+		partida=1
+	elif [[ ${tablero[0]} != 1 && ${tablero[1]} != 2 && ${tablero[2]} != 3 && ${tablero[3]} != 4 && ${tablero[4]} != 5 && ${tablero[5]} != 6 && ${tablero[6]} != 7 && ${tablero[7]} != 8 && ${tablero[8]} != 9 ]]; then
+		partida=1
+	else
+		if [[ $turno -eq 0 ]]; then
+			turno=1
+		else
+			turno=0
+		fi
 	fi
 }
+clear
+echo "Iniciando juego Tateti"
+sleep 2
+clear
 
-while $jugando == true; do
-    
+puntaje=(0 0)
+jugando=0
+inputNumeros=^[0-9]+$
+while [[ $jugando -eq 0 ]]; do
+	tablero=(1 2 3 4 5 6 7 8 9)
+	partida=0
 	turno=0
-	echo "Turno del jugador 1:"
-	echo 
-	
+
 	mostrarTablero
 
-	while $partida == true; do
-
-		
-		while true; do
+	while [[ $partida -eq 0 ]]; do
+		buenInput=0
+		while [[ $buenInput -eq 0 ]]; do
 			echo "Elije una casilla."
 			read casillaMarcada
-			rs=^[0-9]+$
-	
-			if ! [[ $casillaMarcada =~ $rs ]]; then
-				echo "Has introducido un valor incorrecto. Intenta nuevamente."
+
+			if [[ $casillaMarcada =~ $inputNumeros && $casillaMarcada -gt 0 && $casillaMarcada -lt 10 ]]; then
+				buenInput=1
 			else 
-				if [[ $casillaMarcada -lt 1 || $casillaMarcada -gt 9 ]]; then
-					echo "Error. Elije un número entre 1 y 8."
-				else
-					break
-				fi
+				echo "Error. Elije un número entre 1 y 8."
 			fi
 		done
 
 		marcarCasilla $casillaMarcada
 
-		clear
-		sleep 1
-		
-		mostrarTablero
-
 		if [[ $turno -eq 0 ]]; then
-			checkResultado "X" 1
-
-
-			exit 1
-			echo El jugador 1 ha ganado.
-			puntaje1=$(($puntaje1+1))
-			partida=false
-		elif [[ ${tablero[0]} == "O" && ${tablero[1]} == "O" && ${tablero[2]} == "O" || ${tablero[3]} == "O" && ${tablero[4]} == "O" && ${tablero[5]} == "O" || ${tablero[6]} == "O" && ${tablero[7]} == "O" && ${tablero[8]} == "O" || ${tablero[0]} == "O" && ${tablero[3]} == "O" && ${tablero[6]} == "O" || ${tablero[1]} == "O" && ${tablero[4]} == "O" && ${tablero[7]} == "O" || ${tablero[2]} == "O" && ${tablero[5]} == "O" && ${tablero[8]} == "O" || ${tablero[0]} == "O" && ${tablero[4]} == "O" && ${tablero[8]} == "O" || ${tablero[2]} == "O" && ${tablero[4]} == "O" && ${tablero[6]} == "O" ]]; then
-			echo El jugador 2 ha ganado.
-			puntaje2=$(($puntaje2+1))
-			partida=false
-		elif [[ ${tablero[0]} != 1 && ${tablero[1]} != 2 && ${tablero[2]} != 3 && ${tablero[3]} != 4 && ${tablero[4]} != 5 && ${tablero[5]} != 6 && ${tablero[6]} != 7 && ${tablero[7]} != 8 && ${tablero[8]} != 9 ]]; then
-			echo "Empate"
-			partida=false
-		fi
-		
-		if [[ $turno -eq 1 ]]; then
-			echo "Turno del Jugador 2."
+			checkResultado "X"
 		else
-			echo "Turno del Jugador 1."
+			checkResultado "O"
+		fi
+		clear
+		mostrarTablero
+	done
+
+	buenInput=0
+	while [[ $buenInput -eq 0 ]]; do
+		echo "¿Deseas jugar otra partida? S / N"
+		read rta
+		if [ ${rta^^} = S ]; then
+			echo "Partidas ganadas por el J1: ${puntaje[0]}"
+			echo "Partidas ganadas por el J2: ${puntaje[1]}"
+		elif [ ${rta^^} = N ]; then
+			jugando=1
+			buenInput=1
+		else
+			echo "Error. Introduce S / N."
 		fi
 	done
-	
-	echo "¿Deseas jugar otra partida? S / N"
-	read rta
-	if [ ${rta^^} = S ]; then
-		echo "Partidas ganadas por el J1: $p1"
-		echo "Partidas ganadas por el J2: $p2"
-	elif [ ${rta^^} = N ]; then
-		jugando=false
-	else
-		echo "Error. Introduce S / N."
-	fi
 done
 
-echo "Fin del juego. Puntaje final: "
-echo "Jugador 1: $puntaje1"
-echo "Jugador 2: $puntaje2"
+echo "Fin del juego. Puntaje: "
+echo "Jugador 1: ${puntaje[0]}"
+echo "Jugador 2: ${puntaje[1]}"
 
-if [[ $puntaje1 -gt $puntaje2 ]]; then
+if [[ ${puntaje[0]} -gt ${puntaje[1]} ]]; then
 	echo "Ha ganado el Jugador 1."
-elif [[ $puntaje2 -gt $puntaje1 ]]; then
+elif [[ ${puntaje[1]} -gt ${puntaje[0]} ]]; then
 	echo "Ha ganado el Jugador 2."
-elif [[ $puntaje1 -eq $puntaje2 ]]; then
+elif [[ ${puntaje[0]} -eq ${puntaje[1]} ]]; then
 	echo "Empate."
 fi
+exit 0
